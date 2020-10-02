e9dfc6588d5e95db1566e79e8e1c8171069c2301
Arnold Fernandes <afernandes@factset.com>
2020-10-02 13:30:36 -0400

    improving raise-pull-request script logging

--- .github/scripts/raise-pull-request.sh
+++ .github/scripts/raise-pull-request.sh
@@ -21,28 +21,28 @@ remote_branch_check=$(git ls-remote --heads origin $branch_name)
 
 if [ -z "$remote_branch_check" ]
 then
-  echo "new branch"
+  echo "Switching to new branch '$branch_name'"
   git checkout -b $branch_name
   rm -r auto-generated-sdk
   cp -r ../generator/languages/$language/sdk auto-generated-sdk
   git status
   if git diff-index --quiet HEAD -- 
   then 
-    echo "No changes to commit." 
+    echo "No changes to commit" 
   else
     git add -A .
-    git commit -m "feat(sdk): Auto-commit from $generator_repo_name repository PR $pr_number for SDK version v$version"
+    git commit -m "feat(sdk): Auto-commit from '$generator_repo_name' repository PR $pr_number for SDK version v$version"
     echo "Committed all the changes"
     
     git push origin $branch_name
-    echo "Pushed all the changes to remote location"
+    echo "Pushed to remote location"
     
     export GITHUB_USER=$USER
     export GITHUB_TOKEN=$USER_API_KEY
-    hub pull-request -m "feat(sdk): Auto-created from $generator_repo_name repository PR $pr_number for SDK version v$version" -h $branch_name
+    hub pull-request -m "feat(sdk): Auto-created from '$generator_repo_name' repository PR $pr_number for SDK version v$version" -h $branch_name
   fi
 else
-  echo "branch exists"
+  echo "Switching to existing branch '$branch_name'"
   git checkout $branch_name
   git pull
   rm -r auto-generated-sdk
@@ -50,13 +50,13 @@ else
   git status
   if git diff-index --quiet HEAD -- 
   then 
-    echo "No changes to commit." 
+    echo "No changes to commit" 
   else 
     git add -A .
-    git commit -m "feat(sdk): Auto-commit from $generator_repo_name repository PR $pr_number for SDK version v$version"
+    git commit -m "feat(sdk): Auto-commit from '$generator_repo_name' repository PR $pr_number for SDK version v$version"
     echo "Committed all the changes"
   
     git push origin $branch_name
-    echo "Pushed all the changes to remote location"
+    echo "Pushed to remote location"
   fi
 fi
