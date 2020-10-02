e30c7892c2bd6a2518ce56b0f3dae58a96d66b18
afernandes85 <58183178+afernandes85@users.noreply.github.com>
2020-10-02 16:00:08 -0400

    feat(dotnet): upgrade (#11)

--- .github/scripts/raise-pull-request.sh
+++ .github/scripts/raise-pull-request.sh
@@ -2,6 +2,8 @@
 
 language=$1
 
+generator_repo_name=$(jq --raw-output .repository.name "$GITHUB_EVENT_PATH")
+
 version=$(grep -Po '"packageVersion":.*?[^\\]",' generator/languages/$language/openapi-generator-config.json | cut -c20-24)
 if [ -z "$version" ]
 then
@@ -14,33 +16,33 @@ cd sdk
 git config user.email $USER_EMAIL
 git config user.name $USER
 
-branch_name="feat/sdk/generator-pr-$pr_number"
+branch_name="feat/sdk/$generator_repo_name-pr-$pr_number"
 remote_branch_check=$(git ls-remote --heads origin $branch_name)
 
 if [ -z "$remote_branch_check" ]
 then
-  echo "new branch"
+  echo "Switching to new branch '$branch_name'"
   git checkout -b $branch_name
   rm -r auto-generated-sdk
   cp -r ../generator/languages/$language/sdk auto-generated-sdk
   git status
   if git diff-index --quiet HEAD -- 
   then 
-    echo "No changes to commit." 
+    echo "No changes to commit" 
   else
     git add -A .
-    git commit -m "feat(sdk): Auto-commit by Generator PR $pr_number for SDK version v$version"
+    git commit -m "feat(sdk): Auto-commit from '$generator_repo_name' repository PR $pr_number for SDK version v$version"
     echo "Committed all the changes"
     
     git push origin $branch_name
-    echo "Pushed all the changes to remote location"
+    echo "Pushed to remote location"
     
     export GITHUB_USER=$USER
     export GITHUB_TOKEN=$USER_API_KEY
-    hub pull-request -m "feat(sdk): Auto-created by Generator PR $pr_number" -h $branch_name
+    hub pull-request -m "feat(sdk): Auto-created from '$generator_repo_name' repository PR $pr_number for SDK version v$version" -h $branch_name
   fi
 else
-  echo "branch exists"
+  echo "Switching to existing branch '$branch_name'"
   git checkout $branch_name
   git pull
   rm -r auto-generated-sdk
@@ -48,13 +50,13 @@ else
   git status
   if git diff-index --quiet HEAD -- 
   then 
-    echo "No changes to commit." 
+    echo "No changes to commit" 
   else 
     git add -A .
-    git commit -m "feat(sdk): Auto-commit by Generator PR $pr_number for SDK version v$version"
+    git commit -m "feat(sdk): Auto-commit from '$generator_repo_name' repository PR $pr_number for SDK version v$version"
     echo "Committed all the changes"
   
     git push origin $branch_name
-    echo "Pushed all the changes to remote location"
+    echo "Pushed to remote location"
   fi
-fi         
+fi
